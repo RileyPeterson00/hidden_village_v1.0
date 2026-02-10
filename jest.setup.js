dae2f01677faffa65e60f66da18eb5ba6805a405
evanjanderson05 <evanjanderson05@gmail.com>
2026-02-09 18:20:47 -0600

    Created a basic PixiJS mock and a new tests directory. Confirmed Jest and the PixiJS mock were functional through tests

--- jest.setup.js
+++ jest.setup.js
@@ -1,1 +1,25 @@
 import '@testing-library/jest-dom';
+
+// PixiJS mocks
+global.PIXI = {
+  Application: jest.fn().mockImplementation(() => ({
+    stage: { addChild: jest.fn() },
+    renderer: { view: {} },
+  })),
+  Sprite: {
+    from: jest.fn().mockReturnValue({}),
+  },
+  Container: jest.fn().mockImplementation(() => ({
+    addChild: jest.fn(),
+    removeChild: jest.fn(),
+  })),
+  Text: jest.fn().mockImplementation(() => ({
+    text: '',
+    style: {},
+  })),
+  Graphics: jest.fn().mockImplementation(() => ({
+    beginFill: jest.fn(),
+    drawRect: jest.fn(),
+    endFill: jest.fn(),
+  })),
+};
