1ab5b6c09e41b22c200f201f21ce429bc69ed593
evanjanderson05 <evanjanderson05@gmail.com>
2026-02-11 10:58:55 -0600

    Added empty latinSquare testing file, changed Jest config to only search src/tests

--- jest.config.js
+++ jest.config.js
@@ -1,4 +1,5 @@
 module.exports = {
   testEnvironment: 'jsdom', // best environment for testing react components
   setupFilesAfterEnv: ['<rootDir>/jest.setup.js'], // loads setup file before every test
+  roots: ['<rootDir>/src/tests'], // only look into the tests directory for tests
 };
